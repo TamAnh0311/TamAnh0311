dbb756742e1b4a1e0f672b956fb8c783471ebcc6
Tamanh0311 <Tamanh.vu.0311@gmail.com>
2020-05-27 02:34:16 +0700

    adding plugin

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
 module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimizer: [new UglifyJsPlugin({})],
+    },
+    plugins: [
+      new ImageminPlugin({
+        disable: process.env.NODE_ENV !== 'production',
+      })
+    ]
+  },
   publicPath: '/'
 }
