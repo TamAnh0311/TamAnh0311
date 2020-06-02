aa8a9ef817061384858df0a4c46e0a59a6925988
Tamanh0311 <Tamanh.vu.0311@gmail.com>
2020-06-03 04:53:52 +0700

    Minor css fix

---
+++ vue.config.js
@@ -0,0 +1,29 @@
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
+
+module.exports = {
+  configureWebpack: {
+    optimization: {
+      minimizer: [new UglifyJsPlugin({
+        chunkFilter: (chunk) => {
+          if (chunk.name === 'vendor') {
+            return false;
+          }
+          return true;
+        },
+      })],
+    },
+    plugins: [
+      new ImageminPlugin({
+        optipng: {
+          optimizationLevel: 9,
+        },
+        pngquant: {
+          quality: '95-100',
+        },
+        disable: process.env.NODE_ENV !== 'production',
+      }),
+    ],
+  },
+  publicPath: '/',
+};
